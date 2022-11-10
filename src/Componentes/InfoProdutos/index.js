ab94deed6cb033e93a50e34204544f78d222499b
Lucas-Belmiro <103300371+Lucas-Belmiro@users.noreply.github.com>
2022-11-10 07:12:05 -0300

    ajsutes nas paginas

--- src/Componentes/InfoProdutos/index.js
+++ src/Componentes/InfoProdutos/index.js
@@ -1,161 +1,164 @@
 import { useEffect, useState } from "react";
 import IncrmentoQuantidade from "../IncrementoQuantidade";
 import SelecaoTamanho from "../SelecaoTamanho";
-import "./InfoProdutos.css"
+import "./InfoProdutos.css";
 import { Link } from "react-router-dom";
 import InfoExtra from "../InfoExtra";
 import Loading from "../Loading";
 
+const InfoProdutos = (props) => {
+  let productArray = [
+    {
+      name: "Tênis Nike Extend",
+      price: "499.00",
+      color: "Preto",
+      img: "/Imagens/3.png",
+      id: "product1",
+      size: "",
+      quantity: "",
+      numberSales: 0,
+    },
 
-const InfoProdutos = (props)=>{
+    {
+      name: "Tênis Fila Sport",
+      price: "299.00",
+      color: "Preto",
+      img: "/Imagens/2.png",
+      id: "product2",
+      size: "",
+      quantity: "",
+      numberSales: 0,
+    },
 
-    let productArray = [
+    {
+      name: "Tênis Nike Galaxy",
+      price: "529.00",
+      color: "Preto",
+      img: "/Imagens/1.png",
+      id: "product3",
+      size: "",
+      quantity: "",
+      numberSales: 0,
+    },
 
-        {
-            name: "Tênis Nike Extend",
-            price: "499.00",
-            color: "Preto",
-            img: "/imagens/3.png",
-            id: "product1",
-            size: "",
-            quantity: "",
-            numberSales: 0
-        },
-    
-        {
-            name: "Tênis Fila Sport",
-            price: "299.00",
-            color: "Preto",
-            img: "/imagens/2.png",
-            id: "product2",
-            size: "",
-            quantity: "",
-            numberSales: 0
-        },
-    
-        {
-            name: "Tênis Nike Galaxy",
-            price: "529.00",
-            color: "Preto",
-            img: "/imagens/1.png",
-            id: "product3",
-            size: "",
-            quantity: "",
-            numberSales: 0
-        },
-    
-        {
-            name: "Tênis Nike WL",
-            price: "422.00",
-            color: "Branco",
-            img: "/imagens/4.png",
-            id: "product4",
-            size: "",
-            quantity: "",
-            numberSales: 0
-        },
-    
-        {
-            name: "Tênis Nike XP",
-            price: "358.00",
-            color: "Rosa",
-            img: "/imagens/5.jpg",
-            id: "product5",
-            size: "",
-            quantity: "",
-            numberSales: 0
-        },
-    
-        {
-            name: "Tênis Nike Future",
-            price: "522.00",
-            color: "Pink",
-            img: "/imagens/6.jpg",
-            id: "product6",
-            size: "",
-            quantity: "",
-            numberSales: 0
-        },
-    
-        {
-            name: "Tênis Nike World",
-            price: "415.00",
-            color: "Preto",
-            img: "/imagens/7.jpg",
-            id: "product7",
-            size: "",
-            quantity: "",
-            numberSales: 0
-        },
-    
-        {
-            name: "Tênis Addidas Racer",
-            price: "299.00",
-            color: "Preto",
-            img: "/imagens/8.jpg",
-            id: "product8",
-            size: "",
-            quantity: "",
-            numberSales: 0
-    
-        },
-    
-        {
-            name: "Tênis adiddas Dark",
-            price: "359.00",
-            color: "Preto",
-            img: "/imagens/9.jpg",
-            id: "product9",
-            size: "",
-            quantity: "",
-            numberSales: 0
-        }
-    ]
+    {
+      name: "Tênis Nike WL",
+      price: "422.00",
+      color: "Branco",
+      img: "/Imagens/4.png",
+      id: "product4",
+      size: "",
+      quantity: "",
+      numberSales: 0,
+    },
 
-    const [objeto, setObjeto] = useState(""); 
-    const [showLoading, setShowLoading] = useState(true); 
-    
-    useEffect(()=>{
+    {
+      name: "Tênis Nike XP",
+      price: "358.00",
+      color: "Rosa",
+      img: "/Imagens/5.jpg",
+      id: "product5",
+      size: "",
+      quantity: "",
+      numberSales: 0,
+    },
 
-        const idExtistente = JSON.parse(localStorage.getItem("id")) || [];
-        const armazenaObjeto = productArray.find(e=> e.id == idExtistente)
-        setObjeto(armazenaObjeto);
-        setShowLoading(false)
+    {
+      name: "Tênis Nike Future",
+      price: "522.00",
+      color: "Pink",
+      img: "/Imagens/6.jpg",
+      id: "product6",
+      size: "",
+      quantity: "",
+      numberSales: 0,
+    },
 
-    }, [])
+    {
+      name: "Tênis Nike World",
+      price: "415.00",
+      color: "Preto",
+      img: "/Imagens/7.jpg",
+      id: "product7",
+      size: "",
+      quantity: "",
+      numberSales: 0,
+    },
 
+    {
+      name: "Tênis Addidas Racer",
+      price: "299.00",
+      color: "Preto",
+      img: "/Imagens/8.jpg",
+      id: "product8",
+      size: "",
+      quantity: "",
+      numberSales: 0,
+    },
 
-    return (
+    {
+      name: "Tênis adiddas Dark",
+      price: "359.00",
+      color: "Preto",
+      img: "/Imagens/9.jpg",
+      id: "product9",
+      size: "",
+      quantity: "",
+      numberSales: 0,
+    },
+  ];
 
-        <>      
-            <div className="box-principal">
-                <section className="info-produtos">
-                    <div className="info-imagens">
-                        <div>
-                            <img className="produto-principal" src= {objeto.img} ></img>
-                        </div>
+  const [objeto, setObjeto] = useState("");
+  const [showLoading, setShowLoading] = useState(true);
 
-                        <div className="box-imagens">
-                            <img className="imagem-extra" src="/imagens/imagenone.png" ></img>
-                            <img className="imagem-extra" src="/imagens/imagenone.png"></img>
-                            <img className="imagem-extra" src="/imagens/imagenone.png"></img>
-                        </div>
-                    </div>
+  useEffect(() => {
+    const idExtistente = JSON.parse(localStorage.getItem("id")) || [];
+    const armazenaObjeto = productArray.find((e) => e.id == idExtistente);
+    setObjeto(armazenaObjeto);
+    setShowLoading(false);
+  }, []);
 
-                    <div className="info-extra">
-                        <div>
-                            <InfoExtra name = {objeto.name} price = {objeto.price} ></InfoExtra>
-                            <Link to="/carrinho" className="btn btn-primary botao-produto">Adicionar ao carrinho</Link>    
-                        </div>
-                    </div>
-                </section>
-                {showLoading && <Loading></Loading>}   
+  return (
+    <>
+      <div className="box-principal">
+        <section className="info-produtos">
+          <div className="info-imagens">
+            <div>
+              <img className="produto-principal" src={objeto.img}></img>
             </div>
 
-            
+            <div className="box-imagens">
+              <img
+                className="imagem-extra"
+                src={"/Imagens/imagenone.png"}
+                alt="Template de imagem vazia"
+              ></img>
+              <img
+                className="imagem-extra"
+                src={"/Imagens/imagenone.png"}
+                alt="Template de imagem vazia"
+              ></img>
+              <img
+                className="imagem-extra"
+                src={"/Imagens/imagenone.png"}
+                alt="Template de imagem vazia"
+              ></img>
+            </div>
+          </div>
 
-        </>
-    )
-}
+          <div className="info-extra">
+            <div>
+              <InfoExtra name={objeto.name} price={objeto.price}></InfoExtra>
+              <Link to="/carrinho" className="btn btn-primary botao-produto">
+                Adicionar ao carrinho
+              </Link>
+            </div>
+          </div>
+        </section>
+        {showLoading && <Loading></Loading>}
+      </div>
+    </>
+  );
+};
 
 export default InfoProdutos;
--- src/Pages/Home/App.js
+++ src/Pages/Home/App.js
@@ -1,6 +1,6 @@
 import Topo from "../../Componentes/Topo";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useEffect, useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useEffect, useState } from "react";
 import BarraPromocao from "../../Componentes/BarraPromocao";
 import Carrossel from "../../Componentes/Carrossel";
 import Blog from "../../Componentes/Blog";
@@ -8,26 +8,19 @@ import BannerCadastro2 from "../../Componentes/BannerCadastro2";
 import BarraCadastro from "../../Componentes/BarraCadastro";
 import Titulo from "../../Componentes/Titulo";
 import Card from "../../Componentes/Card";
-import "./App.css"
+import "./App.css";
 import Footer from "../../Componentes/Footer";
 
-
-
 function App(props) {
+  const [produtos, setProdutos] = useState([]);
 
-const [produtos, setProdutos] = useState([])
-
-
-useEffect(()=>{
-  fetch(`https://cqokjbs4mc.execute-api.us-east-1.amazonaws.com/dev/produtos`)
-      .then (response => response.json())
-      .then (data => setProdutos(data))
-}, [])
-
+  useEffect(() => {
+    fetch(`https://cqokjbs4mc.execute-api.us-east-1.amazonaws.com/dev/produtos`)
+      .then((response) => response.json())
+      .then((data) => setProdutos(data));
+  }, []);
 
   return (
-
-    
     <div className="App">
       <BarraPromocao></BarraPromocao>
       <Topo></Topo>
@@ -35,24 +28,45 @@ useEffect(()=>{
       <BarraCadastro></BarraCadastro>
       <Titulo nome="OFERTAS"></Titulo>
       <div className="ajusta-produtos">
-        {produtos.map(produto=><Card key = {produto.id} nome = {produto.name} preco = {produto.price} imagem = {produto.img} id={produto.id} ></Card>)}
+        {produtos.map((produto) => (
+          <Card
+            key={produto.id}
+            nome={produto.name}
+            preco={produto.price}
+            imagem={produto.img}
+            id={produto.id}
+          ></Card>
+        ))}
       </div>
       <Titulo nome="BLOG"></Titulo>
-      
-     <section className="ajuste-blog">
-        <Blog nome = "Dicas de cuidados para o seu tênis" descricao = "" imagem = "/imagens/blog1.png"></Blog>
-        <Blog nome = "Saiba como correr com mais conforto" descricao = "" imagem = "/imagens/blog2.png"></Blog>
-        <Blog nome = "Entenda como fazer o alongamento certo" descricao = "" imagem = "/imagens/blog3.png"></Blog>
-        <Blog nome = "Aprenda qual o tênis certo para o seu esporte" descricao = "" imagem = "/imagens/Blog4.jpg" ></Blog>
-     </section>
-      
+
+      <section className="ajuste-blog">
+        <Blog
+          nome="Dicas de cuidados para o seu tênis"
+          descricao=""
+          imagem={"/Imagens/blog1.png"}
+        ></Blog>
+        <Blog
+          nome="Saiba como correr com mais conforto"
+          descricao=""
+          imagem={"/Imagens/blog2.png"}
+        ></Blog>
+        <Blog
+          nome="Entenda como fazer o alongamento certo"
+          descricao=""
+          imagem={"/Imagens/blog3.png"}
+        ></Blog>
+        <Blog
+          nome="Aprenda qual o tênis certo para o seu esporte"
+          descricao=""
+          imagem={"/Imagens/Blog4.jpg"}
+        ></Blog>
+      </section>
+
       <BannerCadastro2></BannerCadastro2>
 
       <Footer></Footer>
-
     </div>
-
-   
   );
 }
 
